850ab80403e1e40ad0887749dbc7f9f3cf64efa9
Trystan <trystandames08@gmail.com>
2021-04-13 10:57:35 +0200

    third commit

--- Blade Frogger/Player/Player.gd
+++ Blade Frogger/Player/Player.gd
@@ -14,3 +14,16 @@ func _process(delta):
 	if Input.is_action_pressed("ui_down"):
 		velocity.y += 1
 	move_and_slide(velocity.normalized() * speed)
+	player_animation(velocity)
+
+func player_animation(velocity):
+	if velocity.y > 0:
+		get_node("AnimatedSprite").play('walk_down')
+	elif velocity.y < 0:
+		get_node("AnimatedSprite").play('walk_up')
+	elif velocity.x != 0:
+		get_node("AnimatedSprite").play('walk_side')
+		get_node("AnimatedSprite").flip_h = velocity.x > 0
+	else:
+		get_node("AnimatedSprite").stop()
+		get_node("AnimatedSprite").frame = 0
