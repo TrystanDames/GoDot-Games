cb160282ad12bb1db5d43dfdfbe4d87caec8e35d
Trystan <trystandames08@gmail.com>
2021-04-14 11:07:58 +0200

    seventh commit

--- Blade Frogger/Car/Car.gd
+++ Blade Frogger/Car/Car.gd
@@ -1,9 +1,16 @@
 extends Area2D
 
 var car_speed = 250
+var direction = 1
+
+func _ready():
+	if position.x < 0:
+		direction = -1
+	else:
+		direction = 1
 
 func _process(delta):
-	position.x -= car_speed * delta
+	position.x -= car_speed * delta * direction
 
 func _on_Car_body_entered(body):
 	get_tree().change_scene("res://GameScreen/GameScreen.tscn")
--- Blade Frogger/Level/Level.gd
+++ Blade Frogger/Level/Level.gd
@@ -4,4 +4,5 @@ const CAR = preload("res://Car/Car.tscn")
 
 func _on_Timer_timeout():
 	var car = CAR.instance()
+	car.position = get_node("CarStart1").position
 	add_child(car)
