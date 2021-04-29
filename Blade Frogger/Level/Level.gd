ed4dd6d6775681bbfa10881d0155245f3884eb31
Trystan <trystandames08@gmail.com>
2021-04-29 10:06:15 +0200

    ninth commit

--- Blade Frogger/Level/Level.gd
+++ Blade Frogger/Level/Level.gd
@@ -2,7 +2,27 @@ extends Node
 
 const CAR = preload("res://Car/Car.tscn")
 
-func _on_Timer_timeout():
+func _ready():
+	$Player.position = $PlayerStartPos.position
+
+func car_spawn(start_pos):
 	var car = CAR.instance()
-	car.position = get_node("CarStart1").position
+	var car_start_node_name = "StartPositions/CarStart" + str(start_pos)
+	car.position = get_node(car_start_node_name).position
 	add_child(car)
+
+
+
+func _on_Timer1_timeout(): car_spawn(1)
+
+
+func _on_Timer2_timeout(): car_spawn(2)
+
+
+func _on_Timer3_timeout(): car_spawn(3)
+
+
+func _on_Timer4_timeout(): car_spawn(4)
+
+
+func _on_Timer5_timeout(): car_spawn(5)
