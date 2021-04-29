e07b3365c6d24d94bdfa1707928aba5e420143ba
Trystan <trystandames08@gmail.com>
2021-04-29 09:13:43 +0200

    eighth commit

--- Blade Frogger/Car/Car.gd
+++ Blade Frogger/Car/Car.gd
@@ -1,11 +1,18 @@
 extends Area2D
 
+const CAR_LIST = ['Grey1', 'Grey2', 'Yellow1','Yellow2']
+
 var car_speed = 250
 var direction = 1
 
 func _ready():
+	randomize()
+	var current_car = CAR_LIST[randi() % CAR_LIST.size()]
+	$"AnimatedSprite".play(current_car)
+	
 	if position.x < 0:
 		direction = -1
+		$"AnimatedSprite".flip_h = true
 	else:
 		direction = 1
 
@@ -14,3 +21,4 @@ func _process(delta):
 
 func _on_Car_body_entered(body):
 	get_tree().change_scene("res://GameScreen/GameScreen.tscn")
+
