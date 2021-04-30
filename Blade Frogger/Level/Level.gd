320624d21924b2dfbdd263ea00275f8ae9ea14d3
Trystan <trystandames08@gmail.com>
2021-04-30 09:32:12 +0200

    tenth commit

--- Blade Frogger/Level/Level.gd
+++ Blade Frogger/Level/Level.gd
@@ -10,19 +10,16 @@ func car_spawn(start_pos):
 	var car_start_node_name = "StartPositions/CarStart" + str(start_pos)
 	car.position = get_node(car_start_node_name).position
 	add_child(car)
-
-
+	
+	var timer_node_name = "CarTimers/Timer" + str(start_pos)
+	get_node(timer_node_name).wait_time = rand_range(1.6, 2.2)
 
 func _on_Timer1_timeout(): car_spawn(1)
 
-
 func _on_Timer2_timeout(): car_spawn(2)
-
 
 func _on_Timer3_timeout(): car_spawn(3)
 
-
 func _on_Timer4_timeout(): car_spawn(4)
-
 
 func _on_Timer5_timeout(): car_spawn(5)
